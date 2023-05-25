55393783c0bfff101f6ea39326526dd8273d041c
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2023-05-25 12:38:02 -0400

    Class Code May 25, 2023 @ 12:35

---
+++ src/wk3/Week3.java
@@ -0,0 +1,50 @@
+package wk3;
+
+public class Week3 {
+
+    public static void main(String[] args) {
+
+        /*
+            Ask the user how many tempeartures they want to enter
+            ensure the value is within 1-7
+
+            if user puts neg # or num gt 7: display error message
+            else
+                depending on value
+                    Enter temp for Monday
+                    Enter temp for Tues
+                    Enter temp for Monday
+
+                    output average temperature
+                    depending on temp for each day
+                        tell user what top they should be wearing
+
+            What to wear should be determined by the following
+            lt 0 : winter coat
+            bt 1-10: light coat
+            bt 11-20: long sleeve
+            gt 20: t-shirt
+
+
+            Enter # of days of week you want to enter?
+                -1
+                    error message
+                3
+                    Enter temp for Monday
+                        10
+                    Enter temp for Tuesday
+                        21
+                    Enter temp for Wednesday
+                        -2
+                  
+                   The average temperature is 9 degrees Celsius
+                   On Monday, you should wear a light coat
+                   On Tuesday, you should wear a t-shirt
+                   On Wednesday, you should wear a winter coat
+
+
+         */
+
+    }
+
+}
