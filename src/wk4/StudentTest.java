569e2d7abab127a9be2703c1ec8233889f2b681d
ProfBlanc <34591333+ProfBlanc@users.noreply.github.com>
2023-06-08 14:10:01 -0400

    Class Code June 8, 2023 @ 14h10

--- src/wk4/StudentTest.java
+++ src/wk4/StudentTest.java
@@ -1,10 +1,34 @@
 package wk4;
 
-import org.junit.Test;
+import org.junit.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StudentTest {
+
+    @BeforeClass
+    public static void beforeAllTests(){
+        System.out.println("Welcome to our testing");
+    }
+    @AfterClass
+    public static void afterAllTests(){
+        System.out.println("Testing has ended");
+    }
+    static int NUM_TESTS = 0;
+    static Student student;
+    @Before
+    public void beforeEachTest(){
+        System.out.println("Running Test # " + (++NUM_TESTS));
+        if(NUM_TESTS < 4)
+            student = new Student();
+    }
+    @After
+    public void afterEachTest(){
+        System.out.println("Ending Test # " + NUM_TESTS);
+    }
 
     @Test
     public void testAssertions(){
@@ -14,8 +38,41 @@ public class StudentTest {
         assertTrue(1 == 1 && 1 < 2);
         assertNull(null);
         assertNotNull("hello");
-        assertEquals(20, 5 * 4 + 2);
+        assertEquals(20, 5 * 4);
         assertNotEquals(20, 3 * 6);
+    }
+
+    @Test
+    public void secondSampleTest(){
+
+        //        System.out.println(1 == 2);
+        assertFalse(1 == 2);
+    }
+    @Test
+    public void testDefaultConstructorFirstName(){
+
+        assertNotNull(student);
+        assertNotNull(student.getName());
+        assertEquals("Student", student.getName());
+        assertTrue(student.getName().length() >= 3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Ben", "Blanc", "Joe", "Mary"})  // arrays of primitive datatypes + strings + objects
+    public void testDefaultConstructorSetNameValidValues(String name){
+
+        student.setName(name);
+        assertEquals(name, student.getName());
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({"Ben,20","Blanc, 25","Mary, 19"})
+    public void testTwoArgConstructor(String name, int age){
+
+        student = new Student(name, age);
+        assertEquals(name, student.getName());
+        assertEquals(age, student.getAge());
     }
 
 }
